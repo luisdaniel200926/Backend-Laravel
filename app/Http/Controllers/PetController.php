e055c8477ae1dfa6c94428fc473f3d9d8b81c24e
Luis <luisdaniel200926@hotmail.com>
2021-10-25 10:04:09 -0500

    User and Pet migrations

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pet $pet)
+    {
+        //
+    }
+}
